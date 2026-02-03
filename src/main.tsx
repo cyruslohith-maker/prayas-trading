fa41bbf9082faaddf0d385eff028b5de0777f736
cyruslohith-maker <cyruslohith@gmail.com>
2026-02-03 09:50:34 +0530

    Refactor main.tsx to use React.StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-// @ts-nocheck
-import { createRoot } from "react-dom/client";
-import App from "./app/App";
-import "./styles/index.css"; 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app/App';
+import './index.css';
 
-const rootElement = document.getElementById("root");
-if (rootElement) {
-  createRoot(rootElement).render(<App />);
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
